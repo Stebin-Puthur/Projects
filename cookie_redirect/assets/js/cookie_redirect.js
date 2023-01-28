68596b9eeed43057e8a3c50c5b81a839f548868c
Stebin-Puthur <121421410+Stebin-Puthur@users.noreply.github.com>
2023-01-28 18:05:17 +0530

    Delete cookie_redirect directory

--- cookie_redirect/assets/js/cookie_redirect.js
+++
@@ -1,8 +0,0 @@
-setInterval(function () {
-    if (document.cookie.indexOf("url") < 0) {
-        window.location.href = cookie_object.redirect_page;
-    }
-    else {
-        console.log("cookie exists")
-    }
-    }, cookie_object.interval_timeout * 1000);
